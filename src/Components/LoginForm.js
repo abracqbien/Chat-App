debceab1dfac8cedc4f2bffdf60fec0242a75b14
Alexandre Bracqbien <abracqbien.pro@laposte.net>
2019-03-07 18:45:15 +0100

    36min tuto

---
+++ src/Components/LoginForm.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { VERIFY_USER } from '../Events'
+
+export default class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            nickname: "",
+            error: ""
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        const { socket } = this.props;
+        const { nickname } = this.state;
+        socket.emit(VERIFY_USER, nickname, this.setUser)
+    }
+
+    handleChange = (e) => {
+        this.setState({nickname: e.target.value})
+    }
+
+    render() {
+        const { nickname, error } = this.state;
+
+        return (
+            <div className="login">
+                <form onSubmit={this.handleSubmit} className="login-form">
+                    <label htmlFor="nickname">
+                        <h2>Got a nickname?</h2>
+                    </label>
+                    <input 
+                        type="text"
+                        ref={(input) => {this.textInput = input}}
+                        id="nickname" 
+                        value={nickname}
+                        onChange={this.handleChange}
+                        placeholder={'My Username'}
+                    />
+                    <div className="error">{error ? error:null}</div>
+                </form>
+            </div>
+        )
+    }
+}
